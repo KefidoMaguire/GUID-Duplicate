f5bbb36ce563748abf3dfc0e5a06c6341435dc75
Kefido <kefido@outlook.de>
2023-12-31 11:08:23 +0100

    Add project files.

---
+++ GUID Duplicate/GuidGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUID_Duplicate
+{
+    static class GuidGenerator
+    {
+        public static Guid GuidGeneratorMethod()
+        {
+             Guid id = Guid.NewGuid();
+             return id;
+        }
+    }
+}
---
+++ GUID Duplicate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GUID_Duplicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
